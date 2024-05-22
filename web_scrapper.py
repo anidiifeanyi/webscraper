866bfe64c96956349bcc5f953d9249340724978c
anidiifeanyi <anidiifeanyi@gmail.com>
2024-05-22 05:46:34 +0100

    final commit

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,9 +1,21 @@
-import requests
 from bs4 import BeautifulSoup
-from dotenv import scraper
+import requests
 
 # Specify the URL to scrap
-url = scraper
+scrap = 'https://zerotomastery.io/courses/'
+url = scrap
 
 # send a request
 r = requests.get(url)
+
+# Get data 
+soup = BeautifulSoup(r.content, 'lxml')
+
+#select the data to scrap
+title = soup.find_all('a', {'class':'general-cardstyles__CardTitle-sc-1cdshrd-3 jtxsIx'})
+
+# display specific data
+for title_text in title:
+  print(title_text.getText())
+  
+  
